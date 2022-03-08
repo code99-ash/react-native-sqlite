90128981e03d1333273ea2204f48764a8c8a79e1
code99-ash <oyetikh1@gmail.com>
2022-03-08 04:00:21 +0100

    having access to moble device contact list and sending message

--- App.js
+++ App.js
@@ -1,28 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
+import { NavigationContainer} from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import { Provider } from 'react-redux';
-import { store } from './store'
 
 const Stack = new createStackNavigator()
 
 import Home from './screens/Home'
-import Create from './screens/Create'
+import Message from './screens/Message'
 
 export default function App() {
   return (
-    <Provider store={store}>
-      <View style={styles.container}>
-        <StatusBar style="auto" />
-        <NavigationContainer>
-          <Stack.Navigator>
-            <Stack.Screen name="Home" component={Home} />
-            <Stack.Screen name="Create" component={Create} />
-          </Stack.Navigator>
-        </NavigationContainer>
-      </View>
-    </Provider>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen 
+          name="Home" 
+          component={Home}
+          options={{
+            title: 'Contacts'
+          }}
+        />
+        <Stack.Screen 
+          name="Message" 
+          component={Message} 
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
@@ -30,7 +31,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    // alignItems: 'center',
-    // justifyContent: 'center',
   },
 });
